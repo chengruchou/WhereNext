a5e564edad0e86569d9311055a5dc29d9248f30a
bpthomson <sses3205@gmail.com>
2026-04-21 19:57:37 +0800

    feat cant search null cat poi by id

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -94,11 +94,11 @@
   const searchPlaceId = async (payload: { passPlaceId: number | null }) => {
     try {
       console.log("Search place Id ", payload.passPlaceId)
-      const res = (await api.post("/poi/id", { id: payload.passPlaceId })).data
+      const res = (await api.post("/poi/id", { id: payload.passPlaceId })).data[0]
       console.log("search place res : ", res)
-      if (res) {
+      if (res && res.category_name) {
         noSearchRes.value = false
-        showSearch.value = res
+        showSearch.value = [res]
       } else {
         noSearchRes.value = true
       }
@@ -148,81 +148,72 @@
 
     <v-navigation-drawer permanent width="600">
       <v-row class="fill-height ma-0">
-        
         <v-col cols="6" class="d-flex flex-column h-100 border-e pa-3 pb-0">
-          
-        <div class="flex-shrink-0 d-flex flex-column">
+          <div class="flex-shrink-0 d-flex flex-column">
             <UserLogin @submit-user-id="fetchUserHistory" />
-            
+
             <HistoryList
               :user-history="userHistory"
               @delete-all="delUserHistory"
               @delete-one="delOneHistory"
-              @focus-place="handleFocusPlace"
-            />
-            
+              @focus-place="handleFocusPlace" />
+
             <SearchPanel
               @submit-search-place-id="searchPlaceId"
               @submit-search-place-cat="searchPlaceCat"
               :no-search-res="noSearchRes"
-              :all-cat="allCat" 
-            />
+              :all-cat="allCat" />
           </div>
 
-          <div class="text-warning text-overline mt-2 mb-0 px-4">
-              Search Results
+          <div class="text-warning text-overline mt-2 mb-0 px-4">Search Results</div>
+
+          <div class="flex-grow-1 overflow-y-auto px-4 pb-4">
+            <div v-if="!showSearch || showSearch.length === 0" class="text-grey text-center mt-4">
+              Awaiting search...
             </div>
 
-            <div class="flex-grow-1 overflow-y-auto px-4 pb-4">
-              
-              <div v-if="!showSearch || showSearch.length === 0" class="text-grey text-center mt-4">
-                Awaiting search...
-              </div>
-
-              <div v-else class="d-flex flex-column">
-                <PlaceCard
-                  v-for="e in showSearch"
-                  :place="e"
-                  :show-add="true"
-                  :index="0"
-                  @submit-add-history="addUserHistory"
-                  @focus-place="handleFocusPlace"
-                  density="compact"
-                />
-              </div>
-
+            <div v-else class="d-flex flex-column">
+              <PlaceCard
+                v-for="e in showSearch"
+                :place="e"
+                :show-add="true"
+                :index="0"
+                @submit-add-history="addUserHistory"
+                @focus-place="handleFocusPlace"
+                density="compact" />
             </div>
+          </div>
         </v-col>
 
         <v-col cols="6" class="d-flex flex-column h-100 pa-3">
-          
           <v-btn
             text="Get next POIs"
             @click="getNextPOI"
             class="mb-4 flex-shrink-0"
             color="primary"
-            :loading="isInferring" 
-          />
-          
+            :loading="isInferring" />
+
           <div class="flex-grow-1 overflow-y-auto pr-1">
             <div class="d-flex flex-column">
-              <PlaceCard 
-                v-for="(e, idx) in showRecommend" 
-                :key="e.raw_poi_id || idx" 
-                :place="e" 
-                :show-add="false" 
-                :index="idx" 
-                @focus-place="handleFocusPlace"
-              />
+              <PlaceCard
+                v-for="(e, idx) in showRecommend"
+                :key="e.raw_poi_id || idx"
+                :place="e"
+                :show-add="false"
+                :index="idx"
+                @focus-place="handleFocusPlace" />
             </div>
           </div>
         </v-col>
-
       </v-row>
     </v-navigation-drawer>
 
     <v-main>
-      <MapCanvas :show-recommend="showRecommend" :show-search="showSearch" :user-hist="userHistPlace" :focused-place="focusedPlace" />
+      <MapCanvas
+        :show-recommend="showRecommend"
+        :show-search="showSearch"
+        :user-hist="userHistPlace"
+        :focused-place="focusedPlace" />
     </v-main>
   </v-app>
 </template>
